27694783f0720dc1c440aba2bc99e60967b6153e
Sergio Tabarini <tabarinisergio@gmail.com>
2014-12-23 11:58:34 -0600

    Cambios en la llamada del Json

--- js/script.js
+++ js/script.js
@@ -1,16 +1,23 @@
-$(document).ready(function () {
-    //Knockout Test
-        var url = "https://www.googleapis.com/youtube/v3/search?key=AIzaSyCZIdvJkrCNDjswEeRtMTbN6B9yEiz2pEo&channelId=UCutAQ7OXuxEZ1Cw3ZPmPOZA&part=snippet&maxResults=10";
-        var viewModel = {};
-        $.getJSON(url, function (data) {
-            viewModel.model = data;
-            ko.applyBindings(viewModel);
-
-        });
-    });
-
-
+id = [];
+stringUrl="";
 $(document).ready(function() {
 	$('.dropdown-toggle').dropdown()
 });
 
+$(document).ready(function($) {
+	$.ajax({
+		url : "https://www.googleapis.com/youtube/v3/search?key=AIzaSyCZIdvJkrCNDjswEeRtMTbN6B9yEiz2pEo&channelId=UCutAQ7OXuxEZ1Cw3ZPmPOZA&part=snippet&maxResults=10&format=json",
+		dataType : "jsonp",
+		success : function(parsed_json) {
+	
+		for (var i = 0; i <= 9; i++) {
+			if (i==9){
+				stringUrl = stringUrl + parsed_json["items"][i]["id"]["videoId"];
+			}else{
+				stringUrl = parsed_json["items"][i]["id"]["videoId"]+ "%2C+" +stringUrl
+			};		
+		};
+		console.log(stringUrl);
+		}
+	});
+});
