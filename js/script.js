83fa31c2565d34fb39867417aacfa8cc6db7525d
Sergio Merida <sergiotabarini@hotmail.com>
2015-01-04 12:54:19 -0600

    Changes in the call

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
-id = [];
 stringUrl="";
+UrlData = [];
+viewCount = [];
 $(document).ready(function() {
 	$('.dropdown-toggle').dropdown()
 });
@@ -17,7 +18,41 @@ $(document).ready(function($) {
 				stringUrl = parsed_json["items"][i]["id"]["videoId"]+ "%2C+" +stringUrl
 			};		
 		};
-		console.log(stringUrl);
+		UrlData.push("https://www.googleapis.com/youtube/v3/videos?part=snippet%2C+statistics&id="+stringUrl+"&maxResults=10&key=AIzaSyCZIdvJkrCNDjswEeRtMTbN6B9yEiz2pEo");
+		console.log(UrlData);
+
 		}
 	});
+	$.ajax({
+		url: UrlData,
+		dataType : "jsonp",
+		success : function(parsed_json) {
+			for (var e = 0; e<=9; e++) {
+				viewCount.push(parsed_json["items"]["statistics"][e]["viewcount"])
+			};
+			
+		}
+	
+	});
+console.log(viewCount);
 });
+
+
+
+
+/*
+var BetterListModel = function () {
+			this.itemToAdd = ko.observable("");
+			this.allItems = ko.observableArray([stringUrl]); // Initial items
+			
+			this.lowerItems = function(){
+				this.allItems.toLowerCase();
+			};
+			this.sortItems = function() {
+				this.allItems.sort(function (a, b) {
+				return a.toLowerCase().localeCompare(b.toLowerCase());
+				});
+			};
+			};
+		ko.applyBindings(new BetterListModel());
+*/
