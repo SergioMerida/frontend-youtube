d242151a1ec224497c81bc7939ec62e5c59dfc7f
Sergio Tabarini <tabarinisergio@gmail.com>
2015-01-08 10:14:51 -0600

    KendoGrid

--- js/script.js
+++ js/script.js
@@ -22,13 +22,12 @@ $(document).ready(function() {
 
 $(document).ready(function($) {
 	$.ajax({
-		url : "https://www.googleapis.com/youtube/v3/search?key=AIzaSyCZIdvJkrCNDjswEeRtMTbN6B9yEiz2pEo&channelId=UCutAQ7OXuxEZ1Cw3ZPmPOZA&part=snippet&maxResults=10&format=json",
+		url : "https://www.googleapis.com/youtube/v3/search?key=AIzaSyCZIdvJkrCNDjswEeRtMTbN6B9yEiz2pEo&channelId=UCutAQ7OXuxEZ1Cw3ZPmPOZA&part=snippet&maxResults=50&format=json",
 		dataType : "jsonp",
 		success : function(parsed_json) {
 	
-		for (var i = 0; i <= 9; i++) {
-			id.push(parsed_json["items"][i]["id"]["videoId"])
-			if (i==9){
+		for (var i = 0; i <= 49; i++) {
+			if (i==49){
 				stringUrl = stringUrl + parsed_json["items"][i]["id"]["videoId"];
 			}else{
 				stringUrl = parsed_json["items"][i]["id"]["videoId"]+ "%2C+" +stringUrl
@@ -40,18 +39,21 @@ $(document).ready(function($) {
 			url: UrlData,
 			dataType : "jsonp",
 			success : function(parsed_jso) {
-				for (var e = 0; e<=9; e++) {
+				for (var e = 0; e<=49; e++) {
 					title.push(parsed_jso["items"][e]["snippet"]["title"])
+					id.push(parsed_jso["items"][e]["id"])
 					description.push(parsed_jso["items"][e]["snippet"]["description"])		
 					viewCount.push(parsed_jso["items"][e]["statistics"]["viewCount"])
 					likeCount.push(parsed_jso["items"][e]["statistics"]["likeCount"])
 					commentCount.push(parsed_jso["items"][e]["statistics"]["commentCount"])
 					image.push(parsed_jso["items"][e]["snippet"]["thumbnails"]["default"]["url"])
 				};			
-			console.log(commentCount)
+			console.log(id)
+			console.log(title)
 			var initialData = []
-			for (var e = 0; e<=9; e++) {
-				initialData.push(({name:title[e], images:image[e], view:viewCount[e], like:likeCount[e], comment:commentCount[e]}))
+			for (var e = 0; e<=49; e++) {
+				initialData.push(({name:title[e], images:image[e], view:viewCount[e], like:likeCount[e], comment:commentCount[e]
+, ids:id[e]}))
 			};
 
 var ViewModel = function(items) {
@@ -85,6 +87,7 @@ var ViewModel = function(items) {
         data: this.items,
         rowTemplate: "rowTmpl",
         useKOTemplates: true,
+        height: 430,
 
         columns: [ 
             {
@@ -103,7 +106,11 @@ var ViewModel = function(items) {
 				title: "Comment Count"
 			},
         ],
-        height: 347
+	pageable: {
+        // we don't set any DataSource here
+        pageSize: 3,
+        buttonCount: 1
+      },
     };
 };
 
@@ -137,7 +144,7 @@ var PagedGridModel = function(items) {
         pageSize: 10
     });
 };
- 
+
 ko.applyBindings(new PagedGridModel(initialData));
 */
 /*
